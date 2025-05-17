56ad3eaeafb4f40dc03a7149dc3a4e2c647760e8
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-05-17 10:43:22 -0300

    form

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './src/componentes/Title/';
+import Form from './src/componentes/form';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title></Title>
+      <Form></Form>
       <StatusBar style="auto" />
     </View>
   );
--- src/componentes/form/PostMsg/index.js
+++ src/componentes/form/PostMsg/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function Title(){
+export default function EnviarMsg(){
     return(
         <View>
             <Text>Publicar aviso</Text>
