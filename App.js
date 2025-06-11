da038badfbe6b1d8dfc013d0fee2f332827a16c7
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-06-11 19:23:01 -0300

    projeto modelo - inicio do projeto

--- App.js
+++ App.js
@@ -13,26 +13,3 @@ export default function App() {
     </>
   );
 }
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Title from './src/componentes/Title/';
-import Form from './src/componentes/form';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Title></Title>
-      <Form></Form>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
